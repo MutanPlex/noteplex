2cdef405220cffee5774d5098d92f67a5cb5b0b6
MutanPlex <barisal54@gmail.com>
2024-11-16 04:12:19 +0300

    Get notes added.

--- client/src/components/Notes.jsx
+++ client/src/components/Notes.jsx
@@ -1,7 +1,12 @@
+import GetNotes from "./GetNotes";
 const Notes = () => {
   return (
     <div>
       <h1>Notes Page</h1>
+
+      <div className="notes-container">
+          <GetNotes />
+      </div>
     </div>
   );
 };
