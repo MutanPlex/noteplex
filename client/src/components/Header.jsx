fefbff020850778b09044111093c875d3f316680
MutanPlex <barisal54@gmail.com>
2024-11-16 04:11:44 +0300

    Switched from flex structure to grid structure

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,21 +2,15 @@ import favicon from "/favicon.ico";
 const Header = () => {
   return (
     <header>
-      <div className="left-container">
-        <div className="logo-container">
-          <a href="/">
-            <img src={favicon} className="logo" alt="NotePlex Logo" />
-          </a>
-          <p>NotePlex</p>
-        </div>
-        <div className="search-container">
-          <i className="fa-solid fa-magnifying-glass search-icon"></i>
-          <input
-            type="text"
-            className="search"
-            placeholder="Search for notes"
-          />
-        </div>
+      <div className="logo-container">
+        <a href="/">
+          <img src={favicon} className="logo" alt="NotePlex Logo" />
+        </a>
+        <p>NotePlex</p>
+      </div>
+      <div className="search-container">
+        <i className="fa-solid fa-magnifying-glass search-icon"></i>
+        <input type="text" className="search" placeholder="Search for notes" />
       </div>
 
       <div className="log-reg-container">
