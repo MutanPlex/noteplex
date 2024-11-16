d560d72e48e7a6b720bf02bae371eb0d62e74230
MutanPlex <barisal54@gmail.com>
2024-11-16 04:11:05 +0300

    File created.

---
+++ client/src/components/AddNote.jsx
@@ -0,0 +1,13 @@
+const AddNote = () => {
+  return (
+    <div>
+      <h1>Add Note</h1>
+
+      <div className="notes-container">
+          
+      </div>
+    </div>
+  );
+};
+
+export default AddNote;
